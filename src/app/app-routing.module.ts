6f71585c22da6420a57a4c5188ce564daa852859
Joey <joey.frimin@gmail.com>
2023-12-07 21:41:59 +0100

    [ADD] app routing : component and pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { QuestionComponent } from './pages/question/question.component';
+import { AnswerComponent } from './pages/answer/answer.component';
+import { EndComponent } from './pages/end/end.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'question', component: QuestionComponent },
+  { path: 'answer', component: AnswerComponent },
+  { path: 'end', component: EndComponent },
+  { path: '**', redirectTo: '/home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { QuestionComponent } from './pages/question/question.component';
+import { AnswerComponent } from './pages/answer/answer.component';
+import { EndComponent } from './pages/end/end.component';
+import { GraphComponent } from './components/graph/graph.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent,
+    AnswerComponent,
+    EndComponent,
+    GraphComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,

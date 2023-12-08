23edb8591e3727beeb1ef970516e5007dd0a0c70
Joey <joey.frimin@gmail.com>
2023-12-08 06:52:25 +0100

    [CLEAN] Home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,28 +1,42 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
+interface Language {
+  code: string;
+  label: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  selectedLanguage: any = localStorage.getItem('selectedLanguage');
+  selectedLanguage: string;
+  languages: Language[] = [
+    { code: 'fr', label: 'Français' }, // French
+    { code: 'en', label: 'English' },  // English
+    { code: 'es', label: 'Español' },  // Spanish
+    { code: 'de', label: 'Deutsch' },  // German
+    { code: 'ru', label: 'Русский' },  // Russian
+    { code: 'ar', label: 'العربية' },  // Arabic
+    { code: 'zh', label: '中文' },      // Chinese
+    { code: 'ja', label: '日本語' }     // Japanese
+  ];
 
-  constructor(
-    private translate: TranslateService,
-  ) {
+  constructor(private translate: TranslateService) {
+    this.selectedLanguage = localStorage.getItem('selectedLanguage') || 'en';
     this.removeQuestions();
   }
 
-  onLanguageChange(){
+  onLanguageChange() {
     localStorage.setItem('selectedLanguage', this.selectedLanguage);
     this.translate.use(this.selectedLanguage);
   }
 
-  removeQuestions() {
+  private removeQuestions() {
     for (let i = 1; i <= 5; i++) {
-      localStorage.removeItem('Question_' + i);
+      localStorage.removeItem(`Question_${i}`);
     }
   }
 }

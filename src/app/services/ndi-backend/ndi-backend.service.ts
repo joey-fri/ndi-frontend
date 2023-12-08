c037da0e34fcdbe2b402bbe0a398dba41ed5bf5d
Joey <joey.frimin@gmail.com>
2023-12-08 06:42:56 +0100

    [FIX@] API Link and design end page

--- src/app/services/ndi-backend/ndi-backend.service.ts
+++ src/app/services/ndi-backend/ndi-backend.service.ts
@@ -10,7 +10,7 @@ export class NdiBackendService {
   constructor(private http: HttpClient) { }
 
   getQuestions(selectedLanguage: string): Observable<any> {
-    const url = 'http://localhost:3000/api/questions/' + selectedLanguage;
+    const url = 'https://www.ndi-deploy.fr/api/questions/' + selectedLanguage;
     const apiKey = '21f1aac7-9965-41f9-b88c-b5a4ed4636cb';
 
     const headers = new HttpHeaders().set('apikey', apiKey);
